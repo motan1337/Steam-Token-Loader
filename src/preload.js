f59aaa163e339880e67ac77dbdbb7687086d99af
motan <74509378+motan1337@users.noreply.github.com>
2026-09-24 04:09:37 +0300

    git cleanup

---
+++ src/preload.js
@@ -0,0 +1,8 @@
+'use strict';
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  status: () => ipcRenderer.invoke('status'),
+  login: (line) => ipcRenderer.invoke('login', line),
+  restartAdmin: () => ipcRenderer.invoke('restart-admin'),
+});
