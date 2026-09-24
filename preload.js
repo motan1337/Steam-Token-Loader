c70739cf35c0a8164ed219cadad9b332e660326f
motan <74509378+motan1337@users.noreply.github.com>
2026-09-24 04:08:44 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  status: () => ipcRenderer.invoke('status'),
-  login: (line) => ipcRenderer.invoke('login', line),
-  restartAdmin: () => ipcRenderer.invoke('restart-admin'),
-});
